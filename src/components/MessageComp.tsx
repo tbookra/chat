c25ff499658e836103a81040ee49248a4d45b010
Tomer Becker <tbookra@gmail.com>
2024-11-10 11:59:05 +0200

    saved

--- src/components/MessageComp.tsx
+++ src/components/MessageComp.tsx
@@ -1,43 +1,54 @@
-import { Message } from '../types/generalTypes'
+import { Message } from "../types/generalTypes";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import CheckTwoToneIcon from '@mui/icons-material/CheckTwoTone';
+import CheckTwoToneIcon from "@mui/icons-material/CheckTwoTone";
 
 interface Props {
-    message: Message
+  message: Message;
 }
 
-const MessageComp = ({message}:Props) => {
+const MessageComp = ({ message }: Props) => {
   return (
     <Card
       sx={{
         minWidth: "100px",
         maxWidth: "240px",
-        pointerEvents:"none",
-        minHeight:"80px",
+        pointerEvents: "none",
+        minHeight: "80px",
         position: "relative",
         m: 3,
         ml: message.sender === "host" ? "5px" : "30px",
         backgroundColor: message.sender === "host" ? "#edc790" : "#95C2D5",
         color: "#fffff0",
-        cursor:"pointer",
-        marginTop:"8px"
+        cursor: "pointer",
+        marginTop: "8px",
       }}
     >
       <CardContent>
-        <Typography gutterBottom variant="subtitle1" sx={{fontSize: 14 }}>
+        <Typography gutterBottom variant="subtitle1" sx={{ fontSize: 14 }}>
           {message.text}
         </Typography>
-        <Typography gutterBottom variant="caption" sx={{fontSize: 6 }}>
+        <Typography gutterBottom variant="caption" sx={{ fontSize: 6 }}>
           {message.timestamp}
         </Typography>
-        <CheckTwoToneIcon style={{ fontSize: 12, color: message.watched ? '#075E54' : "#bcbcbc", marginLeft:"5px" }} />
-        <CheckTwoToneIcon style={{ fontSize: 12, color: message.watched ? '#075E54' : "#bcbcbc", marginLeft:"-5px" }} />
+        <CheckTwoToneIcon
+          style={{
+            fontSize: 12,
+            color: message.watched ? "#075E54" : "#bcbcbc",
+            marginLeft: "5px",
+          }}
+        />
+        <CheckTwoToneIcon
+          style={{
+            fontSize: 12,
+            color: message.watched ? "#075E54" : "#bcbcbc",
+            marginLeft: "-5px",
+          }}
+        />
       </CardContent>
-      
     </Card>
-  )
-}
+  );
+};
 
-export default MessageComp
+export default MessageComp;
